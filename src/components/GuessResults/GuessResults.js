06d8720d7d7a2f2f096c34d8db9019bde102fe8b
Daniel Stopka <stopka.d@gmail.com>
2023-09-03 21:58:42 +0200

    feat: exercise 4 - Game logic

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,21 +1,31 @@
 import React from "react";
 import { range } from "../../utils";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { checkGuess } from "../../game-helpers";
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   const guessRows = Array.from(Array(NUM_OF_GUESSES_ALLOWED).keys());
 
   return (
     <div className="guess-results">
-      {guessRows.map((i) => (
-        <p className="guess" key={i}>
-          {range(0, 5).map((j) => (
-            <span className="cell" key={j}>
-              {guesses[i]?.guess[j]}
-            </span>
-          ))}
-        </p>
-      ))}
+      {guessRows.map((i, index) => {
+        const checkGuessResult = checkGuess(guesses[index]?.guess, answer);
+
+        return (
+          <p className="guess" key={i}>
+            {range(0, 5).map((j) => (
+              <span
+                className={`cell ${
+                  (checkGuessResult && checkGuessResult[j].status) || ""
+                }`}
+                key={j}
+              >
+                {guesses[i]?.guess[j]}
+              </span>
+            ))}
+          </p>
+        );
+      })}
     </div>
   );
 }
