1fa821530db1b212838ab7f31ed6e33715fc513d
Daniel Stopka <stopka.d@gmail.com>
2023-09-03 20:30:06 +0200

    feat: exercise 3 - Guess slots

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 function GuessResults({ guesses }) {
+  const guessRows = Array.from(Array(NUM_OF_GUESSES_ALLOWED).keys());
+
   return (
     <div className="guess-results">
-      {guesses.map(({ id, guess }) => (
-        <p className="guess" key={id}>
-          {guess}
+      {guessRows.map((i) => (
+        <p className="guess" key={i}>
+          {range(0, 5).map((j) => (
+            <span className="cell" key={j}>
+              {guesses[i]?.guess[j]}
+            </span>
+          ))}
         </p>
       ))}
     </div>
